a34d1497fd8044206f19d83139d4fde9b1665470
santipaz19 <santipaz19.02@gmail.com>
2023-07-06 18:21:27 -0300

    contact css

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -2,10 +2,13 @@ import style from './contact.module.css'
 import mail from '../../img/logomail.png'
 import wpp from '../../img/wpplogo.png'
 import { Link } from 'react-router-dom'
+import { useForm, ValidationError } from '@formspree/react';
 
 export default function Contact() {
     const email = 'lpvanstravel@gmail.com'
     const subject = 'Presupuesto'
+    const [state, handleSubmit] = useForm("mzblbzlk");
+
     return (
         <div>
             <div className={style.contenedor}>
@@ -21,25 +24,29 @@ export default function Contact() {
                     <h3 className={style.text}>+54 221-538-3210</h3>
                 </Link>
             </div>
-            <div>
-                <form className={style.form}>
-                    <div >
-                        <label>Nombre: </label>
-                        <input className={style.input} type="text" name="nombre" />
-                    </div>
-                    <div>
-                        <label>Telefono: </label>
-                        <input className={style.input} type="number" name="telefono" />
-                    </div>
-                    <div>
-                        <label>Asunto: </label>
-                        <input className={style.input} type="text" name="asunto" />
-                    </div>
-                    <div>
-                        <label>Mensaje: </label>
-                        <textarea className={style.input} name="nombre" />
-                    </div>
-                </form>
-            </div>
-        </div >)
+            {state.succeeded ? (<div className={style.thk}><h4 >Gracias por comunicarte con nosotros!</h4></div>) : (
+                <div className={style.form}>
+                    <form onSubmit={handleSubmit}>
+                        <div >
+                            <label>Nombre </label>
+                            <input className={style.input} placeholder='Ingrese su nombre' required="" type="text" name="nombre" />
+                        </div>
+                        <div>
+                            <label>Telefono </label>
+                            <input className={style.input} placeholder='Ingrese su telefono' required="" type="number" name="telefono" />
+                        </div>
+                        <div>
+                            <label>Asunto </label>
+                            <input className={style.input} placeholder='Ingrese el asunto' required="" type="text" name="asunto" />
+                        </div>
+                        <div>
+                            <label>¿Como podemos ayudarte? </label>
+                            <textarea className={style.input} placeholder='Ingrese el mensaje' required="" name="message" />
+                        </div>
+                        <button className={style.btn} disabled={state.submitting}>Enviar</button>
+                    </form>
+                </div>
+            )}
+        </div >
+    )
 }
