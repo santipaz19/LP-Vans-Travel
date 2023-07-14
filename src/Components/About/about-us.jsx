5070ba95cf6839caf7c302b158eb38a1e926c29c
santipaz19 <santipaz19.02@gmail.com>
2023-07-14 03:51:52 -0300

    about

--- src/Components/About/about-us.jsx
+++ src/Components/About/about-us.jsx
@@ -1,10 +1,60 @@
+import React from 'react';
 import style from './about.module.css'
-import combi from '../../img/camio.jpg'
+import carrusel1 from '../../img/carrusel1.jpg'
+import carrusel2 from '../../img/carrusel2.jpg'
+import carrusel3 from '../../img/carrusel3.jpg'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 export default function Us() {
+    const options = {
+        autoPlay: true,
+        infiniteLoop: true,
+        showStatus: false,
+        showIndicators: false,
+        showThumbs: false,
+        interval: '1500'
+    };
     return (
         <div>
             <div className={style.contenedor}>
-                <img className={style.img} src={combi} alt='' />
+                <h1 className={style.contacts}>Nosotros</h1>
+            </div>
+            {/* <img className={style.img} src={combi} alt='' /> */}
+            <div className={style.about}>
+                <div className={style.cont}>
+                    <Carousel {...options}>
+                        <div>
+                            <img src={carrusel1} alt="imagen" className={style.img} />
+                        </div>
+                        <div>
+                            <img src={carrusel2} alt="imagen" className={style.img} />
+                        </div>
+                        <div>
+                            <img src={carrusel3} alt="imagen" className={style.img} />
+                        </div>
+                    </Carousel>
+                    <div className={style.overlayContainer}>
+                        <div className={style.overlay}>
+                            <h2 className={style.text}>LP Vans Travel</h2>
+                        </div>
+                    </div>
+
+                </div>
+                <div className={style.info} >
+                    <h4>
+                        Desde el año 2000, en LP Vans Travel nos enorgullece brindar servicios exclusivos de traslado de pasajeros. Contamos con camionetas de 15 y 19 asientos, todas habilitadas por la Comisión Nacional de Regulación del Transporte, para que disfrutes de viajes seguros y comodos.
+                    </h4>
+                    <h4>
+                        Actualmente, brindamos servicios para cubrir distintas necesidades de transporte. Ya sea para eventos especiales, viajes corporativos, mini turismo por Argentina o servicios personalizados para empresas y particulares, estamos aquí para ayudarte.
+                    </h4>
+                    <h4>
+                        En LP Vans Travel valoramos la confianza que depositas en nosotros. Nuestro objetivo es brindarte un servicio de alta calidad, adaptado a tus necesidades.
+                    </h4>
+                    <h4>
+                        Únete a nuestra familia en LP Vans Travel, donde nos esforzamos por superar tus expectativas en cada viaje. ¡Te invitamos a disfrutar de la comodidad y seguridad que ofrecemos en nuestros traslados!
+
+                    </h4>
+                </div>
             </div>
         </div >)
 }
